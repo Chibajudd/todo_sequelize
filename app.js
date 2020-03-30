7da98365e23b29ee876e49b24900d07875c21864
FukeTakumi <t6cd24ac27b@gmail.com>
2020-03-30 20:19:21 +0900

    編集と更新の追加

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //viewsまでのパスを指定
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -78,7 +78,61 @@ app.put('/update/:id',(req,res)=>{
     }
   };
   db.todo.update(params,filter).then((results)=>{
-    res.redirect('/');
+    res.redirect('/categories');
+  });
+  //idを探索する
+  //一致するidの要素に対し、edit.ejsで入力された値にアップデートする
+});
+
+//一覧表示
+app.get('/categories',(req,res)=>{
+  db.category.findAll().then ((results)=>{
+    res.render('categories/index.ejs',{categories:results});
+  });
+});
+
+//作成
+app.post('/categories',(req,res) =>{
+  const params = {
+    name: req.body.categoryName
+  };
+  db.category.create(params).then((results)=>{
+    res.redirect('/categories');
+  });
+});
+
+//削除
+app.delete('/categories/:id',(req,res)=>{
+  const filter = {
+    where:{
+      id: req.params.id
+    }
+  };
+  db.category.destroy(filter).then((results) => {
+    res.redirect('/categories');
+  });
+});
+
+//編集
+app.get('/categories/edit/:id',(req,res)=>{
+  db.category.findByPk(req.params.id).then((results) =>{
+    res.render('categories/edit.ejs',{category:results});
+  });
+  //urlに含まれたidを持つ要素を探索し、それをedit.ejsでレンダリングする
+});
+
+//更新
+app.put('/categories/update/:id',(req,res)=>{
+  const params = {
+    name:req.body.categoryName
+  };
+  const filter = {
+    where:{
+      id:req.params.id
+    }
+  };
+  db.category.update(params,filter).then((results)=>{
+    res.redirect('/categories');
   });
   //idを探索する
   //一致するidの要素に対し、edit.ejsで入力された値にアップデートする
